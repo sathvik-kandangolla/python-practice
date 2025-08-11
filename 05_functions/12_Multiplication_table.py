816d0d66eeeb7b57369e93852d585ace91e20661
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-11 12:09:34 +0530

    19_count_digit_.py

--- 05_functions/12_Multiplication_table.py
+++ 05_functions/12_Multiplication_table.py
@@ -1,7 +1,6 @@
-def multiplication_table(num, i=1):
-    if i > 10:
-        return
-    print(f"{num} x {i} = {num * i}")
-    multiplication_table(num, i + 1)
+def multiplication_table(num):
+    for i in range(1, 11):
+        print(f"{num} x {i} = {num * i}")
 
 multiplication_table(5)
+
