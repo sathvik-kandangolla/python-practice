6aa13098e855478ba31169fc6794434ef82a4a6d
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-20 14:46:45 +0530

    19_remove_duplicates.py

---
+++ 06_strings/19_remove_duplicates.py
@@ -0,0 +1,14 @@
+'''Remove Consecutive Duplicates
+
+Input: "aaabbcddd"
+Expected Output: "abcd"
+'''
+
+def remove_consecutive(s):
+    result = s[0]
+    for i in range(1, len(s)):
+        if s[i] != s[i-1]:
+            result += s[i]
+    return result
+
+print(remove_consecutive("aaabbcddd"))
