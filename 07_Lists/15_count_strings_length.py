f2d2280d6c8b32e931951487a7555a2a9467d2c9
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-21 14:12:13 +0530

    15_count_strings_length.py

--- 07_Lists/15_count_strings_length.py
+++ 07_Lists/15_count_strings_length.py
@@ -1,3 +1,9 @@
+'''Count strings with length ≥ 2 and same first/last character
+
+Input: ['abc', 'xyz', 'aba', '1221']
+Output: 2'''
+
+
 words = ['abc', 'xyz', 'aba', '1221']
 count = 0
 for word in words:
