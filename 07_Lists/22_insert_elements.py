b517d7b2f891296efe58468b63982b108af849b1
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-21 15:17:14 +0530

    22_insert_element.py

---
+++ 07_Lists/22_insert_elements.py
@@ -0,0 +1,8 @@
+'''Insert element at a specified position
+
+Input: [1, 2, 3, 4], element=5, position=2
+Expected Output: [1, 2, 5, 3, 4]'''
+
+numbers = [1, 2, 3, 4]
+numbers.insert(2, 5)
+print(numbers)  
