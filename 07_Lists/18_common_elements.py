ba4df45e25973bcc4896b8176c7b18a3418e9d62
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-21 14:50:00 +0530

    18_common_elements.py

--- 07_Lists/18_common_elements.py
+++ 07_Lists/18_common_elements.py
@@ -1,3 +1,8 @@
+'''Find common elements between two lists
+
+Input: [1, 2, 3, 4], [3, 4, 5, 6]
+Expected Output: [3, 4]'''
+
 list1 = [1, 2, 3, 4]
 list2 = [3, 4, 5, 6]
 print(list(set(list1) & set(list2)))
