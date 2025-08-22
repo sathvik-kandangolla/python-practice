4dcee2d40b50d43c63f699c75634eb4caee79fa8
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-22 10:37:52 +0530

    02_indexing.py

---
+++ 08 _Tuples/02_indexing.py
@@ -0,0 +1,11 @@
+'''
+Description: Print the first and last elements of a tuple using positive and negative indexing.
+Learn how to access tuple elements using both forward and backward indexes, which is essential for tuple manipulation.
+Input: my_tuple = (10, 20, 30, 40, 50)
+Output: 10
+50
+
+'''
+my_tuple = (10, 20, 30, 40, 50)
+print(my_tuple[0])   # First
+print(my_tuple[-1])  # Last
