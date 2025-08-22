a13681bec5bc8f453216531019b547ea5e12620f
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-22 11:28:42 +0530

    05_tuple_slicing.py

---
+++ 08 _Tuples/06_tuple_length.py
@@ -0,0 +1,9 @@
+'''
+Description: Print the number of elements in a tuple using the len() function.
+Knowing the length of tuples is crucial when iterating or validating data.
+Input: t = (10, 20, 30, 40)
+Output: 4
+
+'''
+t = (10, 20, 30, 40)
+print(len(t))
