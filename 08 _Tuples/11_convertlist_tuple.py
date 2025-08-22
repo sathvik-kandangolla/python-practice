f5d2d236ae5c0836c6440643faedbe407ce41209
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-22 12:02:23 +0530

    11_convertlist_tuple

---
+++ 08 _Tuples/11_convertlist_tuple.py
@@ -0,0 +1,12 @@
+'''Description: Convert a list to a tuple and a tuple to a list using built-in functions.
+Mastering conversion between these types is vital for interoperability in Python programs.
+Input: lst = [1, 2, 3], tup = (4, 5, 6)
+Output: Tuple: (1, 2, 3)
+List: [4, 5, 6]
+
+'''
+
+lst = [1, 2, 3]
+tup = (4, 5, 6)
+print(tuple(lst))
+print(list(tup))
