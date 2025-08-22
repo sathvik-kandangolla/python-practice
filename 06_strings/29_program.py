2aa0c7d9383d20ea777f64724992053325558908
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-22 10:04:41 +0530

    29_program.py

--- 06_strings/29_program.py
+++ 06_strings/29_program.py
@@ -4,18 +4,26 @@ Input1: "abc bca dca acb"
 input2: "bca axz 2ab dca"
 Expected Output: "abc acb axz 2ab"'''
 
+# Inputs
 input1 = "abc bca dca acb"
 input2 = "bca axz 2ab dca"
 
-# Split inputs into lists of words
-list1 = input1.split()
-list2 = input2.split()
+# Step 1: Convert the strings into lists of words
+list1 = input1.split()   # ['abc', 'bca', 'dca', 'acb']
+list2 = input2.split()   # ['bca', 'axz', '2ab', 'dca']
 
-# Keep words that are unique to each list
-unique_words = [word for word in list1 + list2 if (word in list1 and word not in list2) ]
+# Step 2: Convert lists to sets (to easily compare unique elements)
+set1 = set(list1)
+set2 = set(list2)
 
+# Step 3: Find words that are only in one of the sets (symmetric difference)
+unique_words = set1.symmetric_difference(set2)
+
+# Step 4: Convert back to string
 output = " ".join(unique_words)
-print(output)
+
+print(output)   # abc acb axz 2ab
+
 
 
 
