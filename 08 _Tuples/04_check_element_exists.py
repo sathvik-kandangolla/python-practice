8d76deac9565b22e37657218e1a7968f178c321f
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-22 11:24:27 +0530

    04_check_element_exists.py

---
+++ 08 _Tuples/04_check_element_exists.py
@@ -0,0 +1,9 @@
+'''
+Description: Check if a specified value exists in a tuple and print the result.
+Use the "in" keyword to efficiently search for elements inside a tuple.
+Input: my_tuple = ('a', 'b', 'c'), Check: 'b'
+Output: True
+
+'''
+my_tuple = ('a', 'b', 'c')
+print('b' in my_tuple)
