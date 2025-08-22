afceee96b9b2cac0d4888ceeccccaf7cc9bdc0cc
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-22 12:50:27 +0530

    17_revrse_tuple.py

---
+++ 08 _Tuples/17_reverse_tuple.py
@@ -0,0 +1,9 @@
+'''
+Description: Reverse the order of items in a tuple and print the result.
+This practice is helpful in problems requiring reverse traversal or reordering.
+Input: t = (10, 20, 30, 40)
+Output: (40, 30, 20, 10)
+
+'''
+t = (10, 20, 30, 40)
+print(t[::-1])
