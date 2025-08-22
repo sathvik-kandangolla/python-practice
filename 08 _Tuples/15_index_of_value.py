5e3e48e4df7db91eb36cac9600a8ceb7f2faeaf1
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-22 12:26:39 +0530

    15_index_of_value.py

---
+++ 08 _Tuples/15_index_of_value.py
@@ -0,0 +1,9 @@
+'''
+Description: Find the index of a specified value in a tuple using the index() method.
+Locating elements within tuples is crucial for data lookup and manipulation.
+Input: t = ("cat", "dog", "rabbit"), Find: "dog"
+Output: 1
+
+'''
+t = ("cat", "dog", "rabbit")
+print(t.index("dog"))
