782cd81cfa482aefddae7265d54859a8c9b8851b
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-22 11:35:20 +0530

    08_repeat_tuple.py

---
+++ 08 _Tuples/08_concatenate_tuples.py
@@ -0,0 +1,10 @@
+'''
+Description: Join two tuples together using the + operator and print the result.
+Concatenation helps combine multiple tuples into a single sequence for further processing.
+Input: t1 = (1, 2), t2 = (3, 4)
+Output: (1, 2, 3, 4)
+
+'''
+t1 = (1, 2)
+t2 = (3, 4)
+print(t1 + t2)
---
+++ 08 _Tuples/09_repeat_tuple.py
@@ -0,0 +1,9 @@
+'''
+Description: Repeat a tuple’s elements multiple times using the * operator.
+Tuple repetition is useful for generating predictable patterns or test data.
+Input: t = (5, 6)
+Output: (5, 6, 5, 6, 5, 6)
+
+'''
+t = (5, 6)
+print(t * 3)
