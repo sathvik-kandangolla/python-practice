8b51d30337547c4a112446cb373055dfed202bd9
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-22 11:40:56 +0530

    10_max_min_tuple.py

---
+++ 08 _Tuples/10_max_min_tuple.py
@@ -0,0 +1,10 @@
+'''
+Description: Find and print the maximum and minimum values in a tuple of numbers.
+This is helpful for statistical analysis and summarizing numeric data in tuples.
+Input: t = (11, 3, 55, 21)
+Output: 55
+3
+'''
+t = (11, 3, 55, 21)
+print(max(t))
+print(min(t))
