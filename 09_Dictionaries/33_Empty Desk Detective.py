52629afe91249ec8c13fc0d003d6d5523a077af9
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-25 15:15:19 +0530

    33_Empty Desk Detective.py

---
+++ 09_Dictionaries/33_Empty Desk Detective.py
@@ -0,0 +1,9 @@
+'''
+You check a colleague's desk (dictionary) for items. If it's empty, print a clear message so you don't miss anything!
+Input: desk = {}
+Expected Output: Dictionary is empty!
+
+'''
+desk = {}
+if not desk:
+    print("Dictionary is empty!")
