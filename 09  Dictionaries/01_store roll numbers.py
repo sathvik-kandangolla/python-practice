572f6a510f75cce328b1389a9730aa44edeefc5b
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-25 10:10:09 +0530

    01_store roll numbers.py

---
+++ 09  Dictionaries/01_store roll numbers.py
@@ -0,0 +1,8 @@
+'''
+Log Session a dictionary to store roll numbers (as keys) and names (as values) for 3 students.
+Input: store roll numbers
+Expected Output: {101: 'Ravi', 102: 'Priya', 103: 'Amit'} 
+
+'''
+students = {101: 'Ravi', 102: 'Priya', 103: 'Amit'}
+print(students)
