d66454b076dfd37d46fba605c47b01e19fc17d8a
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-25 10:49:00 +0530

    09_Update 'Anil' age.py

---
+++ 09_Dictionaries/09_Update 'Anil' age.py
@@ -0,0 +1,9 @@
+'''
+Update 'Anil' age.
+Input: ages = {'Anil': 21, 'Sunita': 20}
+Expected Output: {'Anil': 22, 'Sunita': 20}
+
+'''
+ages = {'Anil': 21, 'Sunita': 20}
+ages['Anil'] = 22
+print(ages)
