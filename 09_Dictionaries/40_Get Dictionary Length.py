64999f6433a00497ebdaf330b5ee5b09d0e621f0
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-25 16:55:31 +0530

    40_Get Dictionary Length.py

---
+++ 09_Dictionaries/40_Get Dictionary Length.py
@@ -0,0 +1,15 @@
+'''
+Find how many key-value pairs are in the dictionary.
+
+Input:
+
+d = {'a': 10, 'b': 20, 'c': 30}
+
+
+Expected Output:
+
+3
+'''
+
+d = {'a': 10, 'b': 20, 'c': 30}
+print(len(d))
