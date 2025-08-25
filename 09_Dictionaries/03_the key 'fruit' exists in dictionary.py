e85609c17a527183f0844856cb25ebf075695eec
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-25 10:20:36 +0530

    03_the key 'fruit' exists in dictionary.py

---
+++ 09_Dictionaries/03_the key 'fruit' exists in dictionary.py
@@ -0,0 +1,8 @@
+'''
+How can you check if the key 'fruit' exists in dictionary d?
+Input: d = {'fruit': 'apple', 'veg': 'carrot'}
+Expected Output: True
+
+'''
+d = {'fruit': 'apple', 'veg': 'carrot'}
+print('fruit' in d)
