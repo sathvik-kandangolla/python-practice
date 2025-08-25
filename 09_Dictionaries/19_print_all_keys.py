ae629ff5ae2060bfd0641f41940277b1ae44d75d
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-25 12:00:04 +0530

    19_remove_all_keys.py

---
+++ 09_Dictionaries/19_print_all_keys.py
@@ -0,0 +1,9 @@
+'''
+Loop and print all keys in d = {'a': 10, 'b': 20, 'c': 30}
+Print all keys.
+Input: d = {'a': 10, 'b': 20, 'c': 30}
+Expected Output: a b c
+
+'''
+for k in d.keys():
+    print(k)
