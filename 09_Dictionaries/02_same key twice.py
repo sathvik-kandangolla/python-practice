7f35cc3d0a9b4af5a88a585bca04f2edfc604007
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-25 10:13:09 +0530

    02_same key twice.py

---
+++ 09_Dictionaries/02_same key twice.py
@@ -0,0 +1,8 @@
+'''
+Suppose you write a dictionary with the same key twice. What value is stored?
+Input: d = {'x': 1, 'y': 2, 'x': 5}
+Expected Output: {'x': 5, 'y': 2}
+
+'''
+d = {'x': 1, 'y': 2, 'x': 5}
+print(d)  
