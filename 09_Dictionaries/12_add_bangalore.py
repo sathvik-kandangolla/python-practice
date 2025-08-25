50bfb2d38e27f6c623bb4ebb823f7cb07d9e4ee6
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-25 11:01:11 +0530

    12_add_bangalore.py

---
+++ 09_Dictionaries/12_add_bangalore.py
@@ -0,0 +1,9 @@
+'''
+Add 'Bangalore': 12000000 to cities = {'Delhi': 18000000, 'Mumbai': 20000000}.
+Add Bangalore.
+Input: cities = {'Delhi': 18000000, 'Mumbai': 20000000}
+Expected Output: {'Delhi': 18000000, 'Mumbai': 20000000, 'Bangalore': 12000000}
+'''
+cities = {'Delhi': 18000000, 'Mumbai': 20000000}
+cities['Bangalore'] = 12000000
+print(cities)
