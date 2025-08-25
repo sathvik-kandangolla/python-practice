dc8171e5965c7394124af525a6b6033bb72012cb
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-25 16:15:08 +0530

    38_Check if Dictionary is Empty.py

---
+++ 09_Dictionaries/38_Check if Dictionary is Empty.py
@@ -0,0 +1,18 @@
+'''
+Check whether a dictionary is empty or not.
+
+Input:
+
+d = {}
+
+
+Expected Output:
+
+Dictionary is empty
+
+'''
+d = {}
+if not d:
+    print("Dictionary is empty")
+else:
+    print("Dictionary is not empty")
