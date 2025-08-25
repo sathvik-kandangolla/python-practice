3fb4faa96080732f79aa48392b21b132d8bb1712
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-25 11:48:53 +0530

    16_remove dictionaries.py

---
+++ 09_Dictionaries/16_remove_dictionary.py
@@ -0,0 +1,9 @@
+'''
+Remove key 'math' from marks = {'math': 80, 'science': 85} using del.
+Input: marks = {'math': 80, 'science': 85}
+Expected Output: {'science': 85}
+
+'''
+marks = {'math': 80, 'science': 85}
+del marks['math']
+print(marks)
