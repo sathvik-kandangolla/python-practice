cf9510ec042a80f946dcc08ea6eea122db91ad9b
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-26 10:08:36 +0530

    03_Maximum and Minimum in a Set.py

--- 10_Sets/02_iterate_over_set.py
+++ 10_Sets/02_iterate_over_set.py
@@ -3,8 +3,6 @@ Iterate over a set in Python
 Story: You want to see every animal in your mini zoo, but the order doesn't matter.
 Input:
 animals = {"lion", "tiger", "bear"}
-expected output:lion
-
 '''
 animals = {"lion", "tiger", "bear"}
 for a in animals:
---
+++ 10_Sets/03_Maximum and Minimum in a Set.py
@@ -0,0 +1,11 @@
+'''
+Python - Maximum and Minimum in a Set
+Story: You played some games. What’s your best and worst score?
+Sample Input:
+1
+scores = {3, 7, 10, 2, 9}
+Sample Output:
+Max: 10
+Min: 2
+
+'''
