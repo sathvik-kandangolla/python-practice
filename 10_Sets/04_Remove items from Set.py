5d0da4fe91bc90ed229706fb0ba2bdf5ce7a6b77
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-26 10:12:14 +0530

    04_Remove items from Set.py

---
+++ 10_Sets/04_Remove items from Set.py
@@ -0,0 +1,13 @@
+'''
+Python - Remove items from Set
+Story: You want to take out an old toy from your toy box.
+Sample Input:
+toys = {"robot", "car", "doll"}
+# remove "doll"
+Sample Output:
+{"robot", "car"}
+
+'''
+toys = {"robot", "car", "doll"}
+toys.remove("doll")   
+print(toys)
