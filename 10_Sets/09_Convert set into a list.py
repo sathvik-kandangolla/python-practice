4c060d35f666e784109c99a6001150688b483044
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-26 10:37:17 +0530

    09_Convert set into a list.py

---
+++ 10_Sets/09_Convert set into a list.py
@@ -0,0 +1,11 @@
+'''
+Python program to convert set into a list
+Story: You pour your collection of badges (set) out into a row (list) to show your friends.
+Sample Input:
+badges = {"star", "moon", "heart"}
+Sample Output:
+["star", "moon", "heart"]
+
+'''
+badges = {"star", "moon", "heart"}
+print(list(badges))
