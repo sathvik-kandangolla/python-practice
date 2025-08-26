1677c7363d9c883241b8f7154af37ec3161d5268
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-26 09:59:24 +0530

    01_size_of_a_set.py

---
+++ 10_Sets/01_size of a Set.py
@@ -0,0 +1,11 @@
+'''
+Find the size of a Set in Python
+Story: You have a bag of unique toys. How many toys do you have?
+Input:
+bag = {"ball", "car", "puzzle", "car", "yo-yo"}
+Sample Output:
+4
+
+'''
+bag = {"ball", "car", "puzzle", "car", "yo-yo"}
+print(len(bag))
