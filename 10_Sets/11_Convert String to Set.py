44a4a628d0f361a397f7f748fd7fab61a6e8448b
sathvik-kandangolla <sathviksandy613@gmail.com>
2025-08-26 10:45:49 +0530

    11_Convert String to Set.py

---
+++ 10_Sets/11_Convert String to Set.py
@@ -0,0 +1,12 @@
+'''
+Python program to convert String to Set
+Story: Find all different letters in your name!
+Sample Input:
+myname = "sathvik"
+Sample Output:
+{'s', 'a', 't', 'h', 'v', 'i','k'}
+
+'''
+
+myname = "sathvik"
+print(set(myname))
